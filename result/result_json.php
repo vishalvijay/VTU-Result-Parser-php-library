7433e47a9f5f8f52d1881e007e1ff5f6df5e9ce0
Vishal Vijay <0vishalvijay0@gmail.com>
2014-06-26 11:29:18 +0530

    http header support added for older version of php

--- result/result_json.php
+++ result/result_json.php
@@ -3,6 +3,18 @@
 	require_once __DIR__ . '/VTUResultParser.php';
 	
 	$result;
+
+  if (!function_exists('http_response_code')){
+    function http_response_code($newcode = NULL){
+      static $code = 200;
+      if($newcode !== NULL){
+        header('X-PHP-Response-Code: '.$newcode, true, $newcode);
+        if(!headers_sent())
+          $code = $newcode;
+      }
+      return $code;
+    }
+  }
 	
 	function showResult(){
 		global $result;
